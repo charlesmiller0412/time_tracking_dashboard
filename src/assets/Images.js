e9367d5f3d2697e4ba9d1ba80b3b93bd1bdc2ea9
Charles Miller <charles.miller.0412@gmail.com>
2022-10-07 02:01:40 -0500

    fonts, variables, and generics added

---
+++ src/assets/Images.js
@@ -0,0 +1,10 @@
+export const IMAGES = {
+    ellipsis: require("./icon-ellipsis.svg").default,
+    exercise: require("./icon-exercise.svg").default,
+    play: require("./icon-play.svg").default,
+    selfCare: require("./icon-self-care.svg").default,
+    social: require("./icon-social.svg").default,
+    study: require("./icon-study.svg").default,
+    work: require("./icon-work.svg").default,
+    jeremy: require("./image-jeremy.png").default,
+};
